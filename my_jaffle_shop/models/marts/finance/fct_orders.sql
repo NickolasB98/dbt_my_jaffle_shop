bd8d0ffa4e5c4aef4302c7259e383067ba81e9a6
NickolasB98 <157819544+NickolasB98@users.noreply.github.com>
2026-01-30 14:28:18 +0100

    test all incremental on schema change strategies, end up using sync_all_columns for development
    append_new_columns is safer for productios as it keeps old and new columns in the final table in snowflake

--- my_jaffle_shop/models/marts/finance/fct_orders.sql
+++ my_jaffle_shop/models/marts/finance/fct_orders.sql
@@ -1,8 +1,18 @@
 {{ config(
     materialized='incremental',
     unique_key='order_id',
-    incremental_strategy='merge'
+    incremental_strategy='merge',
+    on_schema_change='sync_all_columns'
     ) }}
+
+    -- sync_all_columns will delete the no longer existing columns, and add the new columns
+    -- this is useful for when you want to add new columns to your model
+    -- without having to manually update the model
+
+    -- append_new_columns will add the new columns to the model
+    -- without deleting the existing columns
+    -- this is useful for when you want to add new columns to your model
+    -- without losing the existing ones (we would keep all, modified and old columns)
 
 with orders as (
     select * from {{ ref('stg_jaffle_shop__orders')}}
@@ -25,6 +35,7 @@ final as (
         o.order_id,
         o.customer_id,
         o.order_date,
+        o.order_status,
         coalesce(op.amount, 0) as amount
     from orders o 
     left join order_payments op on o.order_id = op.order_id
@@ -36,6 +47,10 @@ select * from final
     -- this filter will only be applied on an incremental run
     -- we add 3 days to the max order date to account for any late arriving orders
     where order_date >= (select dateadd(day, -3, max(order_date)) from {{ this }})
+
+
+    -- trick to backfill the whole table - full refresh
+    -- where 1=1
 {% endif %}
 
 order by order_date desc
