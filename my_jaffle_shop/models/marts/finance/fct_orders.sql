92d953cead503baf4d99581360fa53db94c7ae88
NickolasB98 <157819544+NickolasB98@users.noreply.github.com>
2026-01-30 01:50:01 +0100

    make the fct_orders model incremental, with unique key order_id and incremental strategy = merge

--- my_jaffle_shop/models/marts/finance/fct_orders.sql
+++ my_jaffle_shop/models/marts/finance/fct_orders.sql
@@ -1,4 +1,8 @@
-{{ config(materialized='table') }}
+{{ config(
+    materialized='incremental',
+    unique_key='order_id',
+    incremental_strategy='merge'
+    ) }}
 
 with orders as (
     select * from {{ ref('stg_jaffle_shop__orders')}}
@@ -27,3 +31,9 @@ final as (
 )
 
 select * from final
+{% if is_incremental() %}
+    -- this filter will only be applied on an incremental run
+    where order_date >= (select max(order_date) from {{ this }})
+{% endif %}
+
+order by order_date desc
